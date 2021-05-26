6f18a13260ef1d53b46234b7fa78441004e7d543
Trullo <gtstrullo@gmail.com>
2021-05-26 19:00:11 +0200

    prueba 2

--- app/src/main/java/com/proyectosm/mentalhealthapp/MainActivity.java
+++ app/src/main/java/com/proyectosm/mentalhealthapp/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        b = 1;
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.

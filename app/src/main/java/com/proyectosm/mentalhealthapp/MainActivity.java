dc1366c1de0759824204811aa1294faebe0a2901
Trullo <gtstrullo@gmail.com>
2021-05-26 18:52:13 +0200

    prueba 1

--- app/src/main/java/com/proyectosm/mentalhealthapp/MainActivity.java
+++ app/src/main/java/com/proyectosm/mentalhealthapp/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         Fresco.initialize(this);
 //        FirebaseApp.initializeApp(this);
 
+        int a = 1;
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
